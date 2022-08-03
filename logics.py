6236e9f780076704b3dfe1156e178c8aa54db72c
kotfind <kotfind@yandex.ru>
2022-08-03 20:52:29 +0300

    Upload page

--- logics.py
+++ logics.py
@@ -1,6 +1,8 @@
 import sqlite3
 import os
 import shutil
+import uuid
+from PIL import Image
 
 def init(cfg):
     '''
@@ -14,11 +16,13 @@ def init(cfg):
         shutil.copy(os.path.realpath('.tagserver.cfg'), cfgFile)
     cfg.read(cfgFile)
 
-    global imgDir, thumbDir, dbFile
+    global imgDir, thumbDir, dbFile, imgExtensions, maxThumbSize
     storage = os.path.realpath(os.path.expanduser(cfg['File System']['storage']))
     imgDir = os.path.join(storage, 'img')
     thumbDir = os.path.join(storage, 'thumb')
     dbFile = os.path.join(storage, 'main.db')
+    imgExtensions = cfg['Image']['image extensions'].split()
+    maxThumbSize = tuple(map(int, cfg['Image']['thumb size'].split('x')))
 
     firstRun = not os.path.exists(storage)
 
@@ -33,3 +37,51 @@ def init(cfg):
                 con.executescript(f.read())
             con.commit()
 
+def saveThumb(imgFilename, thumbFilename):
+    image = Image.open(imgFilename)
+    image.thumbnail(maxThumbSize)
+    image = image.convert('RGB')
+    image.save(thumbFilename)
+
+def saveFile(file, tags):
+    ext = os.path.splitext(file.filename)[1].lower()
+    if ext in imgExtensions:
+
+        # Upload and save
+        noExtName = uuid.uuid4().hex
+        imgFilename = noExtName + ext
+        thumbFilename = noExtName + '.jpg'
+
+        fullImgFilename = os.path.join(imgDir, imgFilename)
+        fullThumbFilename = os.path.join(thumbDir, thumbFilename)
+
+        file.save(fullImgFilename)
+        saveThumb(fullImgFilename, fullThumbFilename)
+
+        with sqlite3.connect(dbFile) as con:
+            cur = con.cursor()
+
+            cur.execute('''
+                INSERT INTO files(imgFilename, thumbFilename)
+                VALUES (?, ?)
+            ''', (imgFilename, thumbFilename))
+            fileIdx = cur.lastrowid
+
+            for tag in tags:
+                try:
+                    con.execute('''
+                        INSERT INTO tags(name)
+                        VALUES (?)
+                    ''', (tag,))
+                except sqlite3.IntegrityError: # if tag already exists
+                    pass
+
+                con.execute('''
+                    INSERT INTO fileTags(tagId, fileId)
+                        SELECT tags.id, files.id
+                        FROM tags, files
+                        WHERE tags.name = ?
+                          AND files.id = ?
+                ''', (tag, fileIdx))
+
+            con.commit()
--- tagserver.py
+++ tagserver.py
@@ -9,7 +9,7 @@ import os
 
 os.chdir(os.path.dirname(__file__))
 
-app = flask.Flask(__name__) 
+app = flask.Flask(__name__)
 
 cfg = configparser.ConfigParser()
 logics.init(cfg)
@@ -17,6 +17,19 @@ logics.init(cfg)
 @app.route('/')
 def index():
     return 'Hello, world!'
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    if flask.request.method == 'GET':
+        return flask.render_template('upload.j2')
+
+    file = flask.request.files['img']
+    tags = map(lambda s: s.strip(),
+        flask.request.form['tags'].splitlines())
+
+    logics.saveFile(file, tags)
+
+    return flask.redirect(flask.request.url)
 
 if __name__ == '__main__':
     waitress.serve(app, host='0.0.0.0', port=int(cfg['Network']['port']))
