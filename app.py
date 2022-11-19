29a0bde1db1589a55b37d8d33e872b2bd96660f3
kotfind <kotfind@yandex.ru>
2022-11-19 16:39:56 +0300

    Rewrite cli

--- app.py
+++ app.py
@@ -4,35 +4,16 @@ import logics
 
 import flask
 import waitress
-import configparser 
-import sys
-import getpass
-
-cfg = configparser.ConfigParser()
-logics.init(cfg)
-
-def usage():
-    print('Usage: {} run|adduser'.format(sys.argv[0]))
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        usage()
-        exit(1)
-
-    cmd = sys.argv[1].lower()
-
-    if cmd == 'adduser':
-        print('Adding new user')
-        user = input('User: ').strip()
-        password = getpass.getpass('Password: ').strip()
-        logics.addUser(user, password)
-        exit(0)
-    elif cmd != 'run':
-        usage()
-        exit(1)
 
 app = flask.Flask(__name__, template_folder = logics.static('templates'))
 
+@app.cli.command('adduser')
+def adduser():
+    from getpass import getpass
+    print('Adding new user')
+    user = input('User: ').strip()
+    password = getpass('Password: ').strip()
+    logics.addUser(user, password)
 
 import delete
 import file
@@ -44,6 +25,10 @@ import search
 import taglist
 import upload
 
+import configparser
+cfg = configparser.ConfigParser()
+logics.init(cfg)
+
 app.register_blueprint(delete.bp)
 app.register_blueprint(file.bp)
 app.register_blueprint(filesystem.bp)
@@ -53,10 +38,3 @@ app.register_blueprint(logout.bp)
 app.register_blueprint(search.bp)
 app.register_blueprint(taglist.bp)
 app.register_blueprint(upload.bp)
-
-if __name__ == '__main__':
-    waitress.serve(app,
-        host='0.0.0.0',
-        port=int(cfg['Network']['port']),
-        threads=int(cfg['Network']['threads'])
-    )
